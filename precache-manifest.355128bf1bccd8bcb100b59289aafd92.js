de2b38bd3c9ed74f11f8f98ea39ad79bd7496494
Lib HK <example@example.com>
2019-08-31 03:23:32 -0700

    app v0

---
+++ precache-manifest.355128bf1bccd8bcb100b59289aafd92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91de22ffe4a6f1eb0344106e9b5b95b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "805615b8fe2fd819ce99",
+    "url": "./static/css/main.28c2c89d.chunk.css"
+  },
+  {
+    "revision": "0742473f3303df9fdd5c",
+    "url": "./static/js/2.1ae31e3c.chunk.js"
+  },
+  {
+    "revision": "805615b8fe2fd819ce99",
+    "url": "./static/js/main.26400bf1.chunk.js"
+  },
+  {
+    "revision": "d847092f989e7c70dbd7",
+    "url": "./static/js/runtime~main.173b7445.js"
+  }
+]);
