43f06349d79ff55f306269ca5eceaeadf30e4cd8
Lib HK <example@example.com>
2019-09-05 02:03:30 -0700

    add audio

---
+++ precache-manifest.0c69c68fb7560cbb333b213f1aa79417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a217b987635746432f10e26e8600407c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "00ece5548a0a24393d9f",
+    "url": "./static/css/main.b8629eb7.chunk.css"
+  },
+  {
+    "revision": "0742473f3303df9fdd5c",
+    "url": "./static/js/2.1ae31e3c.chunk.js"
+  },
+  {
+    "revision": "00ece5548a0a24393d9f",
+    "url": "./static/js/main.de9ca9e5.chunk.js"
+  },
+  {
+    "revision": "d847092f989e7c70dbd7",
+    "url": "./static/js/runtime~main.173b7445.js"
+  },
+  {
+    "revision": "c146bea4b6aed8becd5176474ba78f03",
+    "url": "./static/media/bird.c146bea4.png"
+  }
+]);
